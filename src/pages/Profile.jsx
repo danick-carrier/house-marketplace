cc37d1936ff6353b31f228d335e2c547ea3b9eca
Danick Carrier <danickcarrier@outlook.com>
2023-01-30 22:25:18 -0500

    a

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,20 @@
+import {getAuth} from 'firebase/auth';
+import {useEffect, useState} from 'react';
+
+
 function Profile() {
+  const [user, setUser] = useState({})
+
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+    console.log(user)
+  },[]);
+
   return(
-    <>
-    </>
+    <div>
+      {user ? (<h1>{user.displayName}</h1>) : (<h1>Not logged in</h1>)}
+    </div>
   );
 }
 
