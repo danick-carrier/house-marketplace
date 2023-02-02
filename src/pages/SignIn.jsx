08ee1e012f66ffeb47dcc6b9e4132696f36492af
danick-carrier <danickcarrier@outlook.com>
2023-02-01 20:19:46 -0500

    google auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRig
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import {toast} from 'react-toastify';
+import OAuth from '../components/OAuth'
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -84,7 +85,7 @@ function SignIn() {
           </button>
         </div>
       </form>
-      {/* google outh*/}
+      <OAuth />
 
       <Link to="/sign-up" className="registerLink">
         Sign Up Instead
