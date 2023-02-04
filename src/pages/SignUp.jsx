d0134153cfa239dffd15be7676db184c690815c1
danick-carrier <danickcarrier@outlook.com>
2023-02-04 16:01:08 -0500

    querying listings

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -43,7 +43,7 @@ function SignUp() {
 
       navigate('/')
     } catch (error) {
-      toast.error('Something went wrong with registration');
+      toast.error('Something went wrong with registration'); 
     }
   }
 
